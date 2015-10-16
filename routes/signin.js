97213c73a2280648e4ef8a803ea34cadebdf9f61
Michael Espey <espemi01@luther.edu>
2015-10-16 09:21:55 -0500

    not a clue

--- routes/signin.js
+++ routes/signin.js
@@ -1,2 +1,7 @@
 var express = require('express');
 var router = express.Router();
+
+/* GET User page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
