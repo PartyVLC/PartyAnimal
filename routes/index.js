6707cc70df8d247cb402a1b43382ddce42de5581
Ellen Widerski <wideel01@luther.edu>
2015-10-23 16:12:28 -0500

    database stuff

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,43 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('PartyAnimal.db');
+
+router.get('/playlists', function(req,res,next) {
+    db.each("SELECT * FROM Playlist", function(err, row){
+		if (err) {
+			console.log(err);
+		}
+		if (row && !err) {
+			console.log(row.PlaylistID);
+			console.log(row.Name);
+			res.json(row);
+		}
+    });
+});
+
+router.get('/songs', function(req,res,next) {
+    db.all("SELECT * FROM Song", function(err, rows){
+    	var songs = []
+    	rows.forEach(function(row) {
+			if (err) {
+				console.log(err);
+			}
+			if (row && !err) {
+				songs.push(row);
+			}
+		});
+		res.json(songs);
+    });
+});
+
+router.post('/addsong', function(req,res,next) {
+	var stmt = "INSERT into Song (SongId,Title) VALUES ('"+req.body.id+"','"+req.body.title+"')";
+	console.log(stmt);
+	db.run(stmt);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
--- routes/player.js
+++ routes/player.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var restapi = express();
 var router = express.Router();
 var async = require('async');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('PartyAnimal.db');
 
 /* GET player page. */
 router.get('/', function(req, res, next) {
   res.render('player', { title: 'Playing' });
+  
+  //db stuff
+  db.run("CREATE TABLE IF NOT EXISTS Playlist (PlaylistID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT)");
+
+  db.run("CREATE TABLE IF NOT EXISTS Song (SongID TEXT PRIMARY KEY, Title TEXT)");
+
+  db.run("CREATE TABLE IF NOT EXISTS PlaylistSong \
+    (PlaylistID INTEGER, \
+    SongID TEXT, \
+    Score INTEGER, \
+    FOREIGN KEY(PlaylistID) REFERENCES Playlist(PlaylistID), \
+    FOREIGN KEY(SongID) REFERENCES Song(SongID))");
 });
 
 module.exports = router;
