4a3a72e77e86553f01967662abd8d192f69207ea
bobberto1995 <bobberto1995@gmail.com>
2015-10-25 15:46:30 -0500

    Added the css layouts

---
+++ TestPage/script.js
@@ -0,0 +1,14 @@
+function showHideMouseOver(e)
+{
+	e.className="showHideButtMouseOver";
+}
+
+function showHideOnClick(e)
+{
+	e.className="showHideButtClicked";
+}
+
+function showHideReset(e)
+{
+	e.className="showHideButt";
+}
--- public/scripts/playlist.js
+++ public/scripts/playlist.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-	var queue = [{id:"HMUDVMiITOU",score:0},{id:"86khmc6y1yE",score:0},{id:"dQw4w9WgXcQ",score:0},{id:"kbxtYqA6ypM",score:0},{id:"mVHJ6OwTYWc",score:577}];
+	var queue = [{id:"HMUDVMiITOU",score:0},{id:"86khmc6y1yE",score:0},{id:"dQw4w9WgXcQ",score:0},{id:"kbxtYqA6ypM",score:0},{id:"mVHJ6OwTYWc",score:0}];
 	console.log(queue);
 	var playlistGroup = document.getElementById('playlistGroup');
 	for (song in queue) {
@@ -11,28 +11,28 @@ window.onload = function() {
 		console.log(queue[song].id);
 		//item.onclick = function() {songSelect(this)};
 		//item.href="#";
-		
+
 		var downV = document.createElement('button');
 		downV.type='button';
 		downV.className='btn btn-default';
-		downV.onclick= function() {downVote(this.parentNode,new Number(song))};
+		downV.onclick= function() {downVote(this.parentNode)};
 		var downSpan = document.createElement('span');
 		downSpan.id='downVoteButt';
 		downSpan.className='glyphicon glyphicon-thumbs-down';
 		downV.appendChild(downSpan);
-		
+	
 		var score = document.createElement('span');
 		score.innerHTML = queue[song].score;
-		
+
 		var upV = document.createElement('button');
 		upV.type='button';
 		upV.className='btn btn-default';
-		upV.onclick= function() {upVote(this.parentNode,new Number(song))};
+		upV.onclick= function() {upVote(this.parentNode)};
 		var upSpan = document.createElement('span');
 		upSpan.id='upVoteButt';
 		upSpan.className='glyphicon glyphicon-thumbs-up';
 		upV.appendChild(upSpan);
-		
+
 		item.appendChild(downV);
 		item.appendChild(score);
 		item.appendChild(upV);
@@ -43,19 +43,30 @@ window.onload = function() {
 		player.loadVideoById(e.id);
 	}
 	
-	function upVote(e,s)
+	function upVote(e)
 	{
-		queue[s].score++;
-		console.log(queue[s].score);
-		console.log(e.childNodes[2]);
-		e.childNodes[2].innerHTML = queue[s].score;
+		var theScore = parseInt(e.childNodes[2].innerHTML)
+		theScore++;
+		e.childNodes[2].innerHTML = theScore;
+		if(theScore >= 5)
+		{
+			var temp = e;
+			var list = e.parentNode;
+			list.removeChild(e);
+			list.insertBefore(temp,list.childNodes[0]);
+			player.loadVideoById(e.id);
+		}
 	}
 	
-	function downVote(e,s)
+	function downVote(e)
 	{
-		queue[s].score--;
-		console.log(queue[s].score);
-		e.childNodes[2].innerHTML = queue[s].score;
+		var theScore = parseInt(e.childNodes[2].innerHTML)
+		theScore--;
+		e.childNodes[2].innerHTML = theScore;
+		if(theScore <= -5)
+		{
+			e.parentNode.removeChild(e);
+		}
 	}
 
 }
