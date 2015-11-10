b4b9a03555552cc47d6a8be96e19106809e58d54
Michael Espey <espemi01@luther.edu>
2015-11-10 16:17:17 -0600

    updates to api

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var player = require('./routes/player');
+var playlists = require('./routes/playlist')
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/dbcheck', player);
+app.use('/playlist/', playlists);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,2 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('PartyAnimal.db');
+
+router.get('/:id', function(req, res, next) {
+	console.log("Getting your playlist: " + req.params.id);
+	res.render('player', { title: 'Playing Playlist ' + req.params.id });
+});
+
+module.exports = router;
