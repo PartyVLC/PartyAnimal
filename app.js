4573465d8f3bdd30905c7699495165fdce625be9
Michael Espey <espemi01@luther.edu>
2015-11-12 14:54:59 -0600

    updating qr shtuff

--- app.js
+++ app.js
@@ -56,5 +56,32 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var qr = require('qr-image');
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+      var qr_svg = qr.image(iface.address + ':3000', { type: 'svg'});
+      qr_svg.pipe(require('fs').createWriteStream('./public/images/ip_qr.svg'));
+
+    }
+    ++alias;
+  });
+});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -8,39 +8,6 @@ var db = new sqlite3.Database('PartyAnimal.db');
 
 router.get('/', function(req,res,next) {
 
-  var os = require('os');
-  var ifaces = os.networkInterfaces();
-
-  Object.keys(ifaces).forEach(function (ifname) {
-    var alias = 0;
-
-    ifaces[ifname].forEach(function (iface) {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
-        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-        return;
-      }
-
-      if (alias >= 1) {
-        // this single interface has multiple ipv4 addresses
-        console.log(ifname + ':' + alias, iface.address);
-      } else {
-        // this interface has only one ipv4 adress
-        console.log(ifname, iface.address);
-      }
-      ++alias;
-    });
-
-    var options2 = {
-      host: '',
-      port: 3000,
-      path: '/api/ip',
-      method: 'GET'
-    };
-    var ip = http.request(options2);
-    ip.end();
-
-  });
-
   db.run("CREATE TABLE IF NOT EXISTS Playlist (PlaylistID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT)");
 
   db.run("CREATE TABLE IF NOT EXISTS Song (SongID TEXT PRIMARY KEY, Title TEXT)");
@@ -48,36 +15,6 @@ router.get('/', function(req,res,next) {
   db.run("CREATE TABLE IF NOT EXISTS PlaylistSong (PlaylistID INTEGER, SongID TEXT, Score INTEGER, FOREIGN KEY(PlaylistID) REFERENCES Playlist(PlaylistID), FOREIGN KEY(SongID) REFERENCES Song(SongID), PRIMARY KEY (PlaylistID,SongID))");
 
   res.render('player', { title: 'Playing' });
-});
-
-router.get('/api/ip', function(req,res,next) {
-  var os = require('os');
-  var ifaces = os.networkInterfaces();
-  var qr = require('qr-image');
-
-  Object.keys(ifaces).forEach(function (ifname) {
-    var alias = 0;
-
-    ifaces[ifname].forEach(function (iface) {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
-        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-        return;
-      }
-
-      if (alias >= 1) {
-        // this single interface has multiple ipv4 addresses
-        console.log(ifname + ':' + alias, iface.address);
-      } else {
-        // this interface has only one ipv4 adress
-        console.log(ifname, iface.address);
-        var qr_svg = qr.image(iface.address + ':3000', { type: 'svg'});
-        qr_svg.pipe(require('fs').createWriteStream('./public/images/ip_qr.svg'));
-
-      }
-      ++alias;
-    });
-  });
-  res.send();
 });
 
 router.get('/api/getSongs', function(req,res,next) {
