c061da5a0d05acd2a4e5faa10f4c2a67cbfb01f7
Ellen Widerski <wideel01@luther.edu>
2015-11-17 14:46:42 -0600

    no idea

--- routes/dj.js
+++ routes/dj.js
@@ -11,4 +11,8 @@ router.get('/playlist/:id',function(req,res,next){
 	res.render('playlist', { title: 'Playlist' });
 });
 
+router.get('/login',function(req,res,next){
+	res.render('login', { title: 'Log in' });
+});
+
 module.exports = router;
