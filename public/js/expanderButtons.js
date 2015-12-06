ac3a31effbffa352f8efb8a2e458c42b178b18e4
bobberto1995 <bobberto1995@gmail.com>
2015-12-05 21:20:10 -0600

    fix some expander button stuff

--- public/js/expanderButtons.js
+++ public/js/expanderButtons.js
@@ -16,3 +16,27 @@ function expand(expanded, fadedOut, fadedIn, toWidth, toHeight)
     elemFadeIn.style.opacity = 1;
     elemFadeOut.style.opacity = 0;
 }
+
+function betterExpand(expanded, fadedOut, fadedIn, fromWidth, fromHeight, toWidth, toHeight){
+    var elemExpand = document.getElementById(expanded);
+    var elemFadeOut = document.getElementById(fadedOut);
+    var elemFadeIn = document.getElementById(fadedIn);
+    
+    elemWidth = elemExpand.offsetWidth;
+    elemHeight = elemExpand.offsetHeight;
+    
+    if(elemWidth == fromWidth && elemHeight == fromHeight)
+    {
+        elemExpand.style.width = toWidth + "px";
+        elemExpand.style.height = toHeight + "px";
+        elemFadeIn.style.opacity = 1;
+        elemFadeOut.style.opacity = 0;
+    }
+    else
+    {
+        elemExpand.style.width = fromWidth + "px";
+        elemExpand.style.height = fromHeight + "px";
+        elemFadeIn.style.opacity = 0;
+        elemFadeOut.style.opacity = 1;
+    }
+}
