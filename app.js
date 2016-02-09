53ff7f30b8532dd2be721ab89119585517ae52a7
Ellen Widerski <wideel01@Ellens-MacBook-Pro.local>
2016-02-09 15:21:47 -0600

    qr

--- app.js
+++ app.js
@@ -62,30 +62,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var os = require('os');
-var ifaces = os.networkInterfaces();
 var qr = require('qr-image');
 
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      //console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-      var qr_svg = qr.image(iface.address + ':3000', { type: 'svg'});
-      qr_svg.pipe(require('fs').createWriteStream('./public/images/ip_qr.svg'));
-    }
-    ++alias;
-  });
-});
+var qr_svg = qr.image(process.env.prefix+"yourpartyanimal.com/guest/1/1", { type: 'svg'});
+qr_svg.pipe(require('fs').createWriteStream('./public/images/ip_qr.svg'));
 
 module.exports = app;
