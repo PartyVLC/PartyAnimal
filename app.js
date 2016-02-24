1940b1b4aa6193ca396aa3ec53a0a677efa8309a
Michael Espey <espemi01@luther.edu>
2016-02-24 14:06:20 -0600

    fixed logging issues...sorry

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(logger('dev'));
 
 // passport config
 var DJ = require('./models/dj');
