3f95bb09cabed8a1d4df30aacab78a5b8f5d7db0
Michael Espey <espemi01@luther.edu>
2016-02-29 15:33:11 -0600

    cleaning up view dir

--- routes/dj.js
+++ routes/dj.js
@@ -16,7 +16,7 @@ module.exports = function(passport){
 	/* GET login page. */
 	router.get('/', function(req, res) {
     	// Display the Login page with any flash message, if any
-		res.render('dj_index', { message: req.flash('message') });
+		res.render('./dj/dj_index', { message: req.flash('message') });
 	});
 
 	/* Handle Login POST */
@@ -28,7 +28,7 @@ module.exports = function(passport){
 
 	/* GET Registration Page */
 	router.get('/signup', function(req, res){
-		res.render('dj_register',{message: req.flash('message')});
+		res.render('/dj/dj_register',{message: req.flash('message')});
 	});
 
 	/* Handle Registration POST */
@@ -40,7 +40,7 @@ module.exports = function(passport){
 
 	/* GET Home Page */
 	router.get('/home', isAuthenticated, function(req, res){
-		res.render('dj_home', { user: req.user });
+		res.render('/dj/dj_home', { user: req.user });
 	});
 
 	/* Handle Logout */
