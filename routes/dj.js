f72a114f1cc4d116d96e1ac2802f69835d9f2564
Michael Espey <espemi01@luther.edu>
2016-04-05 15:07:19 -0500

    stuff, maybe?

--- routes/dj.js
+++ routes/dj.js
@@ -27,7 +27,7 @@ module.exports = function(passport, db, Playlist, Song){
     /* GET login page. */
     router.get('/', function(req, res) {
         // Display the Login page with any flash message, if any
-        res.render('dj_index', { message: req.flash('message') });
+        res.render('index', { message: req.flash('message') });
     });
 
     /* Handle Login POST */
